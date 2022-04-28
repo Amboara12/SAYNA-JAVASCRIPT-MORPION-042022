e70ebdc205ef7bdb389071f4d4a2588b01233ee4
Amboara12 <98528640+Amboara12@users.noreply.github.com>
2022-04-28 16:18:01 -0700

    finitions

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let valiny = [
 function telo() {
     for (let i = 0; i < valiny.length; i++) {
         if (tableT.includes(valiny[i][0]) && tableT.includes(valiny[i][1]) && tableT.includes(valiny[i][2])) {
-            alert("mety")
+            alert("vous avez gagnez")
         }
         else if (tableO.includes(valiny[i][0] && tableO.includes(valiny[i][2] && tableO.includes(valiny[i][1])))){
             alert("mety2")
@@ -28,34 +28,34 @@ function telo() {
 }
 
 function add0() {
-    table[0].innerHTML = "x";
+    table[0].innerHTML = `${olona}`;
     tableT.push(0);
-    table[5].innerHTML = "o";
+    table[5].innerHTML = `${pc}`;
     tableO.push(5)
     telo()
     
 }
 
 function add1() {
-    table[1].innerHTML = "x";
+    table[1].innerHTML = `${olona}`;
     tableT.push(1);
-    table[4].innerHTML = "o";
+    table[4].innerHTML = `${pc}`;
     tableO.push(4);
     telo()
 }
 
 function add2() {
-    table[2].innerHTML = "x";
+    table[2].innerHTML = `${olona}`;
     tableT.push(2);
-    table[3].innerHTML = "o";
+    table[3].innerHTML = `${pc}`;
     tableO.push(3);
     telo()
 }
 
 function add3() {
-    table[3].innerHTML = "x";
+    table[3].innerHTML = `${olona}`;
     tableT.push(3)
-    table[8].innerHTML = "o";
+    table[8].innerHTML = `${pc}`;
     tableO.push(8)
     telo()
     
@@ -63,44 +63,44 @@ function add3() {
 }
 
 function add4() {
-    table[4].innerHTML = "x";
+    table[4].innerHTML = `${olona}`;
     tableT.push(4)
-    table[7].innerHTML = "o";
+    table[7].innerHTML = `${pc}`;
     tableO.push(7)
     telo()
     
 }
 
 function add5() {
-    table[5].innerHTML = "x";
+    table[5].innerHTML = `${olona}`;
     tableT.push(5)
-    table[6].innerHTML = "o";
+    table[6].innerHTML = `${pc}`;
     tableO.push(6)
     telo()
 }
 
 function add6() {
-    table[6].innerHTML = "x";
+    table[6].innerHTML = `${olona}`;
     tableT.push(6)
-    table[5].innerHTML = "o";
+    table[5].innerHTML = `${pc}`;
     tableO.push(5)
     telo()
     
 }
 
 function add7() {
-    table[7].innerHTML = "x";
+    table[7].innerHTML = `${olona}`;
     tableT.push(7)
-    table[2].innerHTML = "o";
+    table[2].innerHTML = `${pc}`;
     tableO.push(2)
     telo()
     
 }
 
 function add8() {
-    table[8].innerHTML = "x";
+    table[8].innerHTML = `${olona}`;
     tableT.push(8)
-    table[1].innerHTML = "o";
+    table[1].innerHTML = `${pc}`;
     tableO.push(1)
     telo()
     
@@ -117,3 +117,23 @@ table[5].addEventListener("click", add5)
 table[6].addEventListener("click", add6)
 table[7].addEventListener("click", add7)
 table[8].addEventListener("click", add8)
+ 
+let choix = document.getElementById('userChoice')
+
+let choixX = document.getElementById("choixX")
+
+function miafina() {
+    choix.style.display = "none";
+    olona = "X"; 
+    pc = "O"
+}
+function miafina1() {
+    choix.style.display = 'none'
+    olona = "O";
+    pc = "X"
+}
+choixX.addEventListener("click", miafina)
+
+let choixO = document.getElementById("choixO")
+choixO.addEventListener("click", miafina1)
+ let olona , pc
